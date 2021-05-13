d28e2efd5cfd0f2e20e9bba47da004f7f872db83
jorgeleal12 <sistemas@grupoempresarialcyc.com>
2021-05-12 21:44:05 -0500

    update

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { APP_BASE_HREF } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+
+const routes: Routes = [
+ 
+  // { path: '**', component: EmptyRouteComponent },
+  { path: "page1", loadChildren: () => import('./pages/page1/page1.module').then(m => m.Page1Module) },
+  { path: "page2", loadChildren: () => import('./pages/page2/page2.module').then(m => m.Page2Module) },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+  ],
+})
+export class AppRoutingModule { }
---
+++ src/app/empty-route/empty-route.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sigle-spa-empty-route',
+  template: '<h1>hola mundo</h1>',
+})
+export class EmptyRouteComponent {}
