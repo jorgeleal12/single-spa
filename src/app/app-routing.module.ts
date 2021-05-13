a006f659f758c62530e0e1e001b2324c12fbe6d3
jorgeleal12 <sistemas@grupoempresarialcyc.com>
2021-05-12 22:12:13 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
-const routes: Routes = [
- 
-  // { path: '**', component: EmptyRouteComponent },
-  { path: "page1", loadChildren: () => import('./pages/page1/page1.module').then(m => m.Page1Module) },
-  { path: "page2", loadChildren: () => import('./pages/page2/page2.module').then(m => m.Page2Module) },
+const routes: Routes = [{
+  path: 'child1',
+  children: [
+    // { path: '**', component: EmptyRouteComponent },
+    { path: "page1", loadChildren: () => import('./pages/page1/page1.module').then(m => m.Page1Module) },
+    { path: "page2", loadChildren: () => import('./pages/page2/page2.module').then(m => m.Page2Module) },
+  ]
+},
+{
+  path: '**',
+  component: EmptyRouteComponent
+}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: [
-    { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: APP_BASE_HREF, useValue: 'child1' },
   ],
 })
 export class AppRoutingModule { }
